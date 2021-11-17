e03cf0a7286878f6d292f3cd2d53f98e8baa059c
npolyak <nick.polyak@verizon.net>
2021-11-17 18:01:21 -0500

    added localization interface functionality

---
+++ src/ThemingAndL10N/IThemeLoader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NP.ViewModelInterfaces.ThemingAndL10N
+{
+    public interface IThemeLoader
+    {
+        event Action<IThemeLoader> SelectedThemeChangedEvent;
+
+        object? SelectedThemeId { get; set; }
+
+        public IThemeInfo? TheSelectedTheme { get; }
+
+        void AddDictionary(object themeId, Uri resourcesUri, Uri styleUri = null);
+    }
+}
---
+++ src/ThemingAndL10N/ThemeInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NP.ViewModelInterfaces.ThemingAndL10N
+{
+    public interface IThemeInfo
+    {
+        object Id { get; set; }
+
+        Uri ResourceUrl { get; set; }
+
+        Uri StyleUrl { get; set; }
+    }
+}   
